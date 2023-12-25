e760f8b2a71c3c426d04b0093768be2705d87a6f
codeman1o1 <70335544+codeman1o1@users.noreply.github.com>
2023-12-25 20:40:46 +0100

    Bertsite

---
+++ website/app.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>
+		<NuxtPage />
+	</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+	title: "Bert",
+	link: [
+		{
+			rel: "icon",
+			type: "image/svg",
+			href: "/favicon.svg"
+		}
+	]
+})
+</script>
---
+++ website/pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="flex flex-col items-center pt-10">
+		<h1 class="text-3xl font-bold">Bert</h1>
+	</div>
+</template>
